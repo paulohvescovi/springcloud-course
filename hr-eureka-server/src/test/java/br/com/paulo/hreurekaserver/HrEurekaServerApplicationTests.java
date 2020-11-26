ff8eb5fdd17ec52c4760df013807efc2d3394f82
paulo.vescovi <paulo.vescovi@ciass.com.br>
2020-11-26 15:57:54 -0300

    create project eureka server

---
+++ hr-eureka-server/src/test/java/br/com/paulo/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulo.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
