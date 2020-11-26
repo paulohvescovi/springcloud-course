79433f6efa6dd7608f42280b4b62987e1ec1a5e9
paulo.vescovi <paulo.vescovi@ciass.com.br>
2020-11-26 20:11:53 -0300

    create gateway zull project

---
+++ hr-api-gateway-zull/src/test/java/br/com/paulo/hrapigatewayzull/HrApiGatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulo.hrapigatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZullApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
