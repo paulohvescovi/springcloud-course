0a6b0114bfa05d3294aea5e0c7622b367f6e4316
paulo.vescovi <paulo.vescovi@ciass.com.br>
2020-12-02 14:08:55 -0300

    create hr-user project for users authentication

---
+++ hr-user/src/main/java/br/com/paulo/hruser/HrUserApplication.java
@@ -0,0 +1,15 @@
+package br.com.paulo.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HrUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrUserApplication.class, args);
+    }
+
+}
